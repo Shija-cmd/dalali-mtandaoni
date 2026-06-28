7abad35ddeb3fe4836b335be56d85d7131d986f0
Shija-cmd <shijajuma@protonmail.com>
2026-06-28 20:44:10 +0300

    Claudinary upload fixing

--- dalalimtandaoni/settings.py
+++ dalalimtandaoni/settings.py
@@ -1,5 +1,6 @@
 import os
 import cloudinary
+import dj_database_url
 
 from dotenv import load_dotenv
 
@@ -147,12 +148,25 @@ WSGI_APPLICATION = 'dalalimtandaoni.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # Production (Render PostgreSQL)
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
-}
+else:
+    # Local development (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
