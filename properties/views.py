077207263f30ab12d340af58c63a045a5d3c3fd2
Shija-cmd <shijajuma@protonmail.com>
2026-06-28 19:27:43 +0300

    Claudinary upload fixing

--- properties/views.py
+++ properties/views.py
@@ -1193,6 +1193,9 @@ def my_favorites(request):
 @login_required
 def my_profile(request):
 
+    owner = request.user
+    listings = Listing.objects.filter(owner=owner)
+
     if request.method == 'POST':
 
         form = UserProfileForm(
@@ -1203,56 +1206,42 @@ def my_profile(request):
 
         if form.is_valid():
 
-            user = form.save()
+            user = form.save(commit=False)
 
-            profile_picture_url = upload_image_to_cloudinary(
-                form.cleaned_data.get('profile_picture'),
-                'profile_pictures'
-            )
+            profile_picture = form.cleaned_data.get('profile_picture')
 
-            if profile_picture_url:
+            if profile_picture:
+                user.profile_picture_url = upload_image_to_cloudinary(
+                    profile_picture,
+                    'profile_pictures'
+                )
 
-                user.profile_picture_url = profile_picture_url
-                user.save(update_fields=['profile_picture_url'])
+                # Do not save local file on Render
+                user.profile_picture = None
+
+            user.save()
 
             messages.success(
                 request,
                 'Profile updated successfully.'
             )
 
-            return redirect(
-                'my_profile'
-            )
+            return redirect('my_profile')
 
     else:
 
-        form = UserProfileForm(
-            instance=request.user
-        )
-
-    listings = Listing.objects.filter(
-        owner=request.user,
-        is_active=True,
-        is_approved=True,
-
-        availability_status='available'
-    )
-
-    whatsapp_number = format_whatsapp_number(
-        request.user.phone_number
-    )
+        form = UserProfileForm(instance=request.user)
 
     return render(
         request,
         'properties/owner_profile.html',
         {
-            'owner': request.user,
-            'listings': listings,
-            'listing_count': listings.count(),
-            'whatsapp_number': whatsapp_number,
+            'owner': owner,
             'form': form,
+            'listings': listings,
             'is_my_profile': True,
             'can_view_owner_contact': True,
+            'whatsapp_number': format_whatsapp_number(owner.phone_number),
         }
     )
 
